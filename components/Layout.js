7855095e1fd2ad66a07a2362e8aef3eadc2488be
y-takagiiii <takagiyuri0712@gmail.com>
2023-10-15 16:54:23 +0900

    レイアウトを適用

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
+import styles from "./Layout.module.css"
+import utilstyles from "../styles/utils.module.css"
+
 const name = "Takagi Yuri"
 export const siteTitle = "Next.js Blog"
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <img src="/images/profile.png"></img>
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img src="/images/profile.webp" className={`${utilstyles.borderCircle} ${styles.headerImage}`}></img>
+        <h1 className={utilstyles.heading2xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>

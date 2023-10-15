ef90d32f76546dd18059720b82e1c37314d733b7
y-takagiiii <takagiyuri0712@gmail.com>
2023-10-15 16:05:32 +0900

    リンク部分を作成

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -48,6 +49,12 @@ export default function Home() {
             height={37}
             priority
           />
+        </div>
+
+        <div>
+          <h1>
+            <Link href="/posts/firstPost">First Post</Link>
+          </h1>
         </div>
 
         <div className={styles.grid}>

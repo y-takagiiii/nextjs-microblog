e31b2cf36cb04b07dff4935f1dd787d70e4d7e00
y-takagiiii <takagiyuri0712@gmail.com>
2023-11-06 16:37:38 +0900

    getStaticPropsを実装

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,22 @@ import Layout from '@/components/Layout'
 import utilstyles from "../styles/utils.module.css"
 import styles from "../styles/Home.module.css"
 import Link from 'next/link'
+import { getPostsData } from '@/lib/post'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout>
       <section className={utilstyles.headingMd}>
